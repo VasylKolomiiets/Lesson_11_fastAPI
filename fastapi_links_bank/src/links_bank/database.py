fc85dd49bd09252665750096dbbf8c662e4efd31
Vasyl Kolomiets <vasilij.kolomiets@gmail.com>
2024-09-16 18:46:17 +0300

    sqlalchemy_create_sqlite_DB_handmade

--- fastapi_links_bank/src/links_bank/database.py
+++ fastapi_links_bank/src/links_bank/database.py
@@ -3,23 +3,18 @@ from sqlalchemy.orm import sessionmaker
 
 from .settings import settings
 
-
+# sqlite work in one thread - so trick settings amd accurate using
+# each request in new thread and close it after.
 engine = create_engine(
     settings.database_url,
     connect_args={'check_same_thread': False},
 )
 
+# we have to commit `by hands` 
 Session = sessionmaker(
     engine,
     autocommit=False,
     autoflush=False,
 )
 
-
-def get_session():
-    session = Session()
-    try:
-        yield session
-    finally:
-        session.close()
 
---
+++ fastapi_links_bank/src/links_bank/tables.py
@@ -0,0 +1,24 @@
+from sqlalchemy import (
+    Column,
+    Date,
+    ForeignKey,
+    Integer,
+    Float,
+    String,
+    CheckConstraint,
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+class Films(Base):
+    __tablename__ = 'films'
+
+    id_4 = Column(Integer, primary_key=True)
+    film_title = Column(String)
+    year = Column(Integer, CheckConstraint("year BETWEEN 1900 AND 2100"))
+    category = Column(String)
+    my_rate = Column(Float)
