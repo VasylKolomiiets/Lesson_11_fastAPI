7fd452a2193dab0b381b1c00155b99518932554c
Vasyl Kolomiets <vasilij.kolomiets@gmail.com>
2024-09-16 12:12:59 +0300

    get_film_by_id

---
+++ main_03.py
@@ -0,0 +1,45 @@
+from typing import NewType
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field, constr, conint
+
+from uuid import UUID, uuid4
+
+app = FastAPI()
+
+
+class Film(BaseModel):
+    idu4: UUID | None = None
+    film_title: constr(min_length=2, max_length=20, pattern="^[ а-щА-ЩЬьЮюЯяЇїІіЄєҐґ']*$")
+    year: int = Field(..., gt=1900, lt=2500, description="Рік виходу фільму", example=1930)
+    category: constr(min_length=4, max_length=20)
+    my_rate: conint(ge=0, le=10) = 0
+
+DBDict = NewType('DBDict', dict[UUID, Film]) 
+
+films: DBDict = {}
+
+@app.post("/films/", response_model=Film)
+def create_film(film: Film):
+    film.idu4 = uuid4()
+    films[film.idu4] = film
+    return film
+
+
+@app.get("/films/", response_model=DBDict)
+def read_films():
+    return films
+
+
+@app.get("/films/{film_id}", response_model=Film)
+def read_task(film_id: UUID):
+    if film_id in films:
+            return films[film_id]
+
+    raise HTTPException(status_code=404, detail="Фільм з таким ключем не знайдено...")
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, port=8000)
