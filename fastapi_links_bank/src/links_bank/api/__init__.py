3d036d3db58188b64f3cdd7d46d8269757ac9e4c
Vasyl Kolomiets <vasilij.kolomiets@gmail.com>
2024-09-16 19:55:22 +0300

    films_router_moved_to_films

--- fastapi_links_bank/src/links_bank/api/__init__.py
+++ fastapi_links_bank/src/links_bank/api/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from . import films
 
 router = APIRouter()
-
+router.include_router(films.router)
--- fastapi_links_bank/src/links_bank/app.py
+++ fastapi_links_bank/src/links_bank/app.py
@@ -1,12 +1,6 @@
 from fastapi  import FastAPI
 
-from .app import router
+from .api import router
 
 app = FastAPI()
 app.include_router(router)
-
-@app.get("/")
-def test_root():
-    return {"test": "passed_well"}
-
-
